5728944948bf95688c9c51588d633bac13aee9ee
brandongatlin <brandongatlin1981@me.com>
2017-10-07 16:27:11 -0500

    added random number generator

--- assets/javascript/psychic.js
+++ assets/javascript/psychic.js
@@ -1,17 +1,16 @@
 //create array of letter choices and answers
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-	console.log(letters[9]);
+	console.log(letters[5]);
 
 //declare computerGuess var
-var computerGuess = "";
 
 //have computer choice random letter and store in variable computerGuess
-function getRandomLetter() {
-    var computerGuess = letters[Math.floor(Math.random() * (letters.length))];
-    console.log(computerGuess);
+var computerGuess = Math.floor(Math.random() * letters.length);
 
-      }  
+	console.log(computerGuess);
+
+
 
 
 
