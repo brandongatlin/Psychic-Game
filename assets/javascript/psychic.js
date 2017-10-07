0661f609ecc2738396ada3da39c27a80ec7d54e1
brandongatlin <brandongatlin1981@me.com>
2017-10-07 18:31:01 -0500

    tried copying over some code - did not work - restored

--- assets/javascript/psychic.js
+++ assets/javascript/psychic.js
@@ -1,30 +1,52 @@
 //create array of letter choices and answers
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-	console.log(letters[5]);
+console.log(letters[5]);
+
+// counter variables
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var playerGuesses = [];
+var computerGuess = "";
+
 
 //declare computerGuess var
-
-//have computer choice random letter and store in variable computerGuess
+//have computer choose random letter and store in variable computerGuess
 var computerGuess = Math.floor(Math.random() * letters.length);
 
-	console.log(computerGuess);
-
-
-
+console.log(computerGuess);
 
 
 //player guesses letter, var playerGuess, print this variable to "your guesses so far"
+var playerGuess = document.getElementById("playerGuesses");
 
+document.onkeyup = function(event) {
+    playerGuess.textContent = event.key;
+    console.log(playerGuess);
+};
+
+
+
+// test
+// make append
+$("letters").on("click", function() {
+    var buttonPressed = $(this).attr("value");
+    $("#playerGuesses").append(playerGuess);
+});
 
 
 //if statement: if playerGuess != to computerGuess, decrease number of guess by 1
 
-
+//playerGuess != computerGuess {
+//stop game, decrease letter guesses by 1
+//}
 
 //if statement: if playerGuess === computerGuess, stop, increase wins +1
-
+//playerGuess === computerGuess {
+//stop game, incrase wins by 1
+//}
 
 
 //
-
