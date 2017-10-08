da0b40494d92f9c886d7d656d8ecd431ecf40f78
brandongatlin <brandongatlin1981@me.com>
2017-10-07 19:44:27 -0500

    got guesses to display as string, instead of replacing each other - used .join

--- assets/javascript/psychic.js
+++ assets/javascript/psychic.js
@@ -11,26 +11,53 @@ var guessesLeft = 9;
 var playerGuesses = [];
 var computerGuess = "";
 
+console.log(guessesLeft);
 
-//declare computerGuess var
-//have computer choose random letter and store in variable computerGuess
-var computerGuess = Math.floor(Math.random() * letters.length);
+//set all to default
+var reset = function() {
+    totalGuesses = 9;
+    guessesLeft = 9;
+    playerGuesses = [];
 
-console.log(computerGuess);
+    updatecomputerGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+}
 
+var updateGuessesLeft = function() {
+        // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+        document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
 
-//player guesses letter, var playerGuess, print this variable to "your guesses so far"
-var playerGuess = document.getElementById("playerGuesses");
-
-document.onkeyup = function(event) {
-    playerGuess.textContent = event.key;
-    console.log(playerGuess);
+var updatecomputerGuess = function() {
+  this.computerGuess = this.computerGuess[Math.floor(Math.random() * this.letters.length)];
+};
+var updateGuessesSoFar = function() {
+  // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+  document.querySelector("#playerGuesses").innerHTML = "Your Guesses so far: " + playerGuesses.join(', ');
 };
 
 
+        //When key is released it becomes the users guess
+        document.onkeyup = function(event) {
+            guessesLeft--;
+            var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-// make append
-$("letters").on("click", function() {
-    var buttonPressed = $(this).attr("value");
-    $("#playerGuesses").append(playerGuess);
-});
+            playerGuesses.push(playerGuess);
+            updateGuessesLeft();
+            updateGuessesSoFar();
+
+            if (guessesLeft > 0) {
+                if (playerGuess === computerGuess) {
+                    wins++;
+                    document.querySelector("#wins").innerHTML = "Wins: " + wins;
+                    reset();
+                }
+            } else if (guessesLeft === 0) {
+                // Then we will loss and we'll update the html to display the loss 
+                losses++;
+                document.querySelector("#losses").innerHTML = "Losses: " + losses;
+                // Then we'll call the reset. 
+                reset();
+            }
+        };
