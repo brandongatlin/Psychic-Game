5234dbe5fcbb5cab78dccce8fa682917f43db456
brandongatlin <brandongatlin1981@me.com>
2017-10-07 20:27:40 -0500

    fixed win counter by rearranging code to read variable right. Still getting error about cannot read property at of coputerGuess

--- assets/javascript/psychic.js
+++ assets/javascript/psychic.js
@@ -1,12 +1,7 @@
 //create array of letter choices and answers
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-console.log(letters[5]);
-
-var computerGuess = letters[Math.floor(Math.random() * letters.length)];
-
-console.log(computerGuess);
-
+	console.log(letters[5]);
 
 // counter variables
 var wins = 0;
@@ -16,7 +11,23 @@ var guessesLeft = 9;
 var playerGuesses = [];
 var computerGuess = "";
 
-console.log(guessesLeft);
+	console.log(guessesLeft);
+
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
+	console.log(computerGuess);
+
+var updateGuessesLeft = function() {
+    document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+
+var updatecomputerGuess = function() {
+    this.computerGuess = this.computerGuess[Math.floor(Math.random() * this.letters.length)];
+};
+
+var updateGuessesSoFar = function() {
+    document.querySelector("#playerGuesses").innerHTML = playerGuesses.join(', ');
+};
 
 //set all to default
 var reset = function() {
@@ -29,39 +40,28 @@ var reset = function() {
     updateGuessesSoFar();
 }
 
-var updateGuessesLeft = function() {
-	document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
+
+
+//When key is released it becomes the users guess
+document.onkeyup = function(event) {
+    guessesLeft--;
+    var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    playerGuesses.push(playerGuess);
+    updateGuessesLeft();
+    updateGuessesSoFar();
+
+    if (guessesLeft > 0) {
+        if (playerGuess === computerGuess) {
+            wins++;
+            document.querySelector("#wins").innerHTML = "Wins: " + wins;
+            reset();
+        }
+    }
+
+    if (guessesLeft === 0) {
+        losses++;
+        document.querySelector("#losses").innerHTML = "Losses: " + losses;
+        reset();
+    }
 };
-
-var updatecomputerGuess = function() {
-  	this.computerGuess = this.computerGuess[Math.floor(Math.random() * this.letters.length)];
-};
-
-var updateGuessesSoFar = function() {
-  	document.querySelector("#playerGuesses").innerHTML = playerGuesses.join(', ');
-};
-
-
-        //When key is released it becomes the users guess
-        document.onkeyup = function(event) {
-            guessesLeft--;
-            var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-            playerGuesses.push(playerGuess);
-            updateGuessesLeft();
-            updateGuessesSoFar();
-
-            if (guessesLeft > 0) {
-                if (playerGuess === computerGuess) {
-                    wins++;
-                    document.querySelector("#wins").innerHTML = "Wins: " + wins;
-                    reset();
-                }
-            } 
-
-            if (guessesLeft === 0) {
-                losses++;
-                document.querySelector("#losses").innerHTML = "Losses: " + losses;
-                reset();
-            }
-        };
